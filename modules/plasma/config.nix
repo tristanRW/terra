ab4c096f2869d66eefec39b4cf03a5367dfef7a4
tristan.rw <_@github.com>
2024-05-06 11:31:24 +0200

    added sound & bluetooth support to "plasma" module

--- modules/plasma/config.nix
+++ modules/plasma/config.nix
@@ -11,6 +11,8 @@ in {
     inherit (lib) mkIf;
     cfg = config.terra.desktop.plasma;
   in mkIf cfg.enable{
+
+    #enable plasma desktop
     services.desktopManager = {
       plasma6 = {
         enable = true;
@@ -20,6 +22,45 @@ in {
       sddm = {
         enable = true;
         wayland.enable = true;
+      };
+    };
+
+    #enable sound
+    security.rtkit.enable = true; #recommended by wiki
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+      wireplumber = {
+        enable = true; #enable session-manager for bluetooth
+        configPackages = [
+          (pkgs.writeTextDir "share/wireplumber/bluetooth.lua.d/51-bluez-config.lua" ''
+            bluez_monitor.properties = {
+              ["bluez5.enable-sbc-xq"] = true,
+              ["bluez5.enable-msbc"] = true,
+              ["bluez5.enable-hw-volume"] = true,
+              ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+            }
+          '')
+        ]; #configure codecs to be used in bt
+      };
+    };
+    #hardware.pulseaudio = {
+    #  enable = true;
+    #  package = pkgs.pulseaudioFull;
+    #}; #enable additional codecs like LDAC and AAC
+
+    #enable bluetooth
+    hardware.bluetooth = {
+      enable = true; # enables support for Bluetooth
+      powerOnBoot = true; # powers up the default Bluetooth controller on boot
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+        }; # This enables A2DP-Sink support for headsets (higgquality sound), "Enable" is indeed correct its not a nix key
       };
     };
 
